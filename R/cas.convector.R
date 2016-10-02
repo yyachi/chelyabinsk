0ef98d7bf42ac9843ac5defebe7235b33f714f39
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2016-10-02 16:17:16 +0900

    Minor tweak

--- R/cas.convector.R
+++ R/cas.convector.R
@@ -3,9 +3,9 @@
 #'   "none","g/g","wt%","cg/g","%","permil","mg/g","ppm","ug/g","ppb","ng/g","pg/g"
 #' @return Conversion cofficient.  When `permil', this returns 1000
 #'   instead of 0.001
-#' @examples
 #' @export
-#' cas.convector("permil")
+#' @examples
+#' > cas.convector("permil")
 cas.convector <- function(tableunit) {
   convector        <- c(1,     1,    100,  100,   100,1000,    1000,  1000000,1000000,1000000000,1000000000,1000000000000)
   names(convector) <- c("none","g/g","wt%","cg/g","%","permil","mg/g","ppm",  "ug/g", "ppb",     "ng/g",    "pg/g")
