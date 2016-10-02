2d59e0f92bfee74a3f3bdc427e0c358d7a84e3fe
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2016-10-02 15:28:18 +0900

    Add cas.convector

---
+++ R/cas.convector.R
@@ -0,0 +1,12 @@
+#' Return conversion cofficient depending on `unit'.
+#' @param tableunit Unit string, that is one of
+#'   ("none","g/g","wt%","cg/g","%","permil","mg/g","ppm","ug/g","ppb","ng/g","pg/g")
+#' @return Conversion cofficient.  When `permil', this returns 1000 instead of 0.001
+#' @export
+#' @examples
+#' cas.convector("permil")
+cas.convector <- function(tableunit) {
+  convector        <- c(1,     1,    100,  100,   100,1000,    1000,  1000000,1000000,1000000000,1000000000,1000000000000)
+  names(convector) <- c("none","g/g","wt%","cg/g","%","permil","mg/g","ppm",  "ug/g", "ppb",     "ng/g",    "pg/g")
+  return(convector[tableunit]);
+}
