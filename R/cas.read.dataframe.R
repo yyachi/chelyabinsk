548f358b26cad71ae85dfa9f3c70108e616697c7
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2016-10-02 11:20:16 +0900

    Try to develop @examples

--- R/cas.read.dataframe.R
+++ R/cas.read.dataframe.R
@@ -1,12 +1,14 @@
-#' Convert dataframe with label to vector with label
+#' Read dataframe created from casteml with column `unit'
 #' @param dataframefile A flipped csvfile
 #' @param tableunit Preferred unit, that is one of c("none","g/g","wt%","cg/g","%","permil","mg/g","ppm","ug/g","ppb","ng/g","pg/g")
 #' @return A dataframe
 #' @export
 #' @examples
+#' \dontrun{
 #' $ casteml download -R 20130528105235-594267 > 20130528105235-594267.pml
 #' $ casteml convert -f dataframe -c trace 20130528105235-594267.pml > 20130528105235-594267.dataframe
 #' R> tbl0 <- cas.read.dataframe("20130528105235-594267.dataframe","ppm")
+#' }
 cas.read.dataframe <- function(dataframefile,tableunit="none"){
   #' ----------------
   #'* unit definition (should be defined as function in the future)
