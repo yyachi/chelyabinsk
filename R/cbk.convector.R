8d1f257c69d1f8384134dc517af5b8053ae1be63
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2016-10-04 08:26:05 +0900

    Add comment

--- R/cbk.convector.R
+++ R/cbk.convector.R
@@ -2,7 +2,7 @@
 #' @param tableunit Unit string, that is one of
 #'   none, g/g, wt\%, cg/g, \%, permil, mg/g, ppm, ug/g, ppb, ng/g, pg/g
 #' @return Conversion cofficient.  When "permil", this returns 1000
-#'   instead of 0.001
+#'   instead of 0.001.  Very likely you want to DIVIDE table by the factor
 #' @export
 #' @examples
 #' cbk.convector("permil")
--- R/cbk.exec.R
+++ R/cbk.exec.R
@@ -1,12 +1,14 @@
-#' Open file even on Mac.  This is copy from
-#' http://stackoverflow.com/questions/12273346/system-independent-method-of-opening-afile
-#' @param x A filename
-#' @return result
+#' Open file even on Mac.  
+#' @param file A filename
+#' @return result by opening the file
 #' @export
-cbk.exec <- function(x){
+cbk.exec <- function(file){
+  # Idea is from
+  # http://stackoverflow.com/questions/12273346/system-independent-method-of-opening-afile
+
   # replacement for shell.exe (doesn't exist on MAC)
   if (exists("shell.exec",where = "package:base"))
-    return(base::shell.exec(x))
-  comm <- paste("open",x)
+    return(base::shell.exec(file))
+  comm <- paste("open",file)
   return(system(comm))
 }
