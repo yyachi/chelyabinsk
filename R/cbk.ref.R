7d132b59c3bab64a3b6396880438e8e9aa33d8a2
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2016-10-04 20:18:20 +0900

    Convert ref.csv to ref1.dataframe to have unit column

--- R/cbk.ref.R
+++ R/cbk.ref.R
@@ -8,13 +8,13 @@
 #' cbk.ref("Wasson 1988",cbk.periodic('atomicnumber'))
 #' cbk.ref("McDonough 1995",cbk.periodic('volatility'))
 #' @export
-#' @importFrom utils read.table
 cbk.ref <- function(analysis,property=NULL){
   ## foo        <- c(.31,.808,.122,.6,.195,.0735,.2590,.0474,.322,.0718,.21,0.0324,.209,.0322)
   ## names(foo) <- c("La","Ce","Pr","Nd","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu")
 
   ### read as vector
-  castbl        <- read.table(cbk.path("ref.csv"),sep=",",header=T,row.names=1)
+  ## castbl     <- read.table(cbk.path("ref.csv"),sep=",",header=T,row.names=1)
+  castbl        <- cbk.read.dataframe(cbk.path("ref1.dataframe"),"ppm")
   ## foo        <- as.numeric(castbl)
   foo           <- as.numeric(castbl[analysis,]) # conversion to numeric vector
   names(foo)    <- names(castbl)
