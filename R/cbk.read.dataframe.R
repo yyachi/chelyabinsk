59f8a99f60bbdd061f3f9c69e051200f9b42ec81
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2016-10-04 13:03:14 +0900

    Add minor doc

--- R/cbk.read.dataframe.R
+++ R/cbk.read.dataframe.R
@@ -1,6 +1,6 @@
 #' @title Read csvfile created by casteml convert with column `unit'
 #'
-#' @description Read csvfile created by \code{casteml convert} with
+#' @description Read csvfile created by \code{casteml convert --format dataframe} with
 #'   column `unit'.
 #'
 #' @details This internally calls
@@ -13,10 +13,12 @@
 #' @return A dataframe with unit organized
 #' @seealso \url{http://dream.misasa.okayama-u.ac.jp}, \code{\link{cbk.convector}}
 #' @export
+#' @examples
+#' cbk.read.dataframe(cbk.path("20130528105235-594267_trace.dataframe"),"ppm")
 cbk.read.dataframe <- function(csvfile,tableunit){
   ### EXAMPLES
   ### $ casteml download -R 20130528105235-594267 > 20130528105235-594267.pml
-  ### $ casteml convert -f dataframe -c trace 20130528105235-594267.pml > 20130528105235-594267.dataframe
+  ### $ casteml convert -f dataframe -c trace 20130528105235-594267.pml > 20130528105235-594267_trace.dataframe
   ### R> tbl0 <- cbk.read.dataframe("20130528105235-594267.dataframe","ppm")
   tblin <- read.csv(csvfile,row.names=1,header=T,stringsAsFactors=F)
   if ('unit' %in% colnames(tblin)) {
