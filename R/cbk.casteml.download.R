481af14224367824387ec4a9d692c3ab67c4c891
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2016-10-13 08:33:35 +0900

    Add hint without changing logic

--- R/cbk.casteml.download.R
+++ R/cbk.casteml.download.R
@@ -6,11 +6,14 @@
 cbk.casteml.download <- function(stone) {
   ## EXAMPLES
   ## stone <- c("20160627191317-464538","20160627191900-040404","20160627191919-895636")
-  ## cbk.casteml.download("stone")
+  ## cbk.casteml.download(stone)
+  ## cbk.casteml.download("20160627191317-464538")
   outfiles <- c()
   for (ii in 1:length(stone)) {
     pmlfile <- tempfile(pattern = paste(stone[ii],".",sep=""), fileext = ".pml")
     system(paste("casteml download",stone[ii],">",pmlfile))
+    ## pmlbuf  <- system(paste("casteml download",stone[ii]), intern = TRUE)
+    ## save(pmlbuf, pmlfile)
     outfiles <- c(outfiles,pmlfile)
   }
   return(outfiles)
