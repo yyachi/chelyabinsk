910b7856d24a8c57b0b980d85cd8cf2f10bcdb1f
Masahiro Yamanaka <yamanaka@cc.okayama-u.ac.jp>
2016-10-12 12:19:42 +0900

    Add download and convert commands

---
+++ R/cbk.casteml.download.R
@@ -0,0 +1,16 @@
+#' Download analysis record in temporary directory and return the file path
+#' 
+#' @param ID Unique indentification number of stones in Medusa
+#' @return file path to CASTEML file that was downloaded in temporary directory
+#' @examples
+#' stone <- c("20160627191317-464538","20160627191900-040404","20160627191919-895636")
+#' cbk.casteml.download("stone")
+cbk.casteml.download <- function(stone) {
+  outfiles <- c()
+  for (ii in 1:length(stone)) {
+    pmlfile <- tempfile(pattern = paste(stone[ii],".",sep=""), fileext = ".pml")
+    system(paste("casteml download",stone[ii],">",pmlfile))
+    outfiles <- c(outfiles,pmlfile)
+  }
+  return(outfiles)
+}
