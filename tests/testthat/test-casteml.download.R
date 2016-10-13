3fe5537234e9b227120bb87aa6e245575d32a162
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2016-10-13 18:06:13 +0900

    Adapt to current cbk.casteml.download.R and cbk.casteml.convert.R

--- tests/testthat/test-casteml.download.R
+++ tests/testthat/test-casteml.download.R
@@ -1,5 +1,5 @@
 test_that("cbk.casteml.download(stone) should return path of pmlfile",{
   stone <- c("20160627191317-464538","20160627191900-040404")
   pmlfile <- cbk.casteml.download(stone)
-  expect_true(file.exists(pmlfile[1]))
+  expect_true(file.exists(pmlfile))
 })
