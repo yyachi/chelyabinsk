1268c7d3a685d01ef4a497ec77f5c760df9aec84
Masahiro Yamanaka <yamanaka@cc.okayama-u.ac.jp>
2016-10-13 18:02:09 +0900

    Wrote tests for cbk.casteml.convert and cbk.casteml.download

---
+++ tests/testthat/test-casteml.convert.R
@@ -0,0 +1,7 @@
+test_that("cbk.casteml.convert(pmlfile) should return path of converted file",{
+  stone <- c("20160627191317-464538","20160627191900-040404")
+  pmlfile <- cbk.casteml.download(stone)
+  outfile <- cbk.casteml.convert(pmlfile[1])
+  expect_true(file.exists(outfile))
+})
+
