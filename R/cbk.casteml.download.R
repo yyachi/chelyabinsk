71c6f8f64b3e741a00a2f8dce9619087ad51b7f3
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2016-10-13 11:49:44 +0900

    Minor tweak

--- R/cbk.casteml.download.R
+++ R/cbk.casteml.download.R
@@ -1,16 +1,18 @@
 #' Download analysis record in temporary directory and return the file path
 #' 
-#' @param ID Unique indentification number of stones in Medusa
-#' @return file path to CASTEML file that was downloaded in temporary directory
+#' @param arguments Arguments that will pass to `casteml download'.
+#'   Typically unique indentification number of stones in Medusa.
+#' @return file path to CASTEML file that was downloaded in temporary
+#'   directory
 #' @export
-cbk.casteml.download <- function(stone) {
+cbk.casteml.download <- function(arguments) {
   ## EXAMPLES
-  ## stone <- c("20160627191317-464538","20160627191900-040404","20160627191919-895636")
-  ## cbk.casteml.download(stone)
+  ## arguments <- c("20160627191317-464538","20160627191900-040404","20160627191919-895636")
+  ## cbk.casteml.download(arguments)
   ## cbk.casteml.download("20160627191317-464538")
-  outfile <- tempfile(pattern = paste(stone[1],"@",sep=""), fileext = ".pml")
-  ## system(paste("casteml download",stone[ii],">",outfile))
-  cmd     <- paste("casteml download",stone)
+  outfile <- tempfile(pattern = paste(arguments[1],"@",sep=""), fileext = ".pml")
+  ## system(paste("casteml download",arguments[ii],">",outfile))
+  cmd     <- paste("casteml download",arguments)
   cat(system(cmd, intern = TRUE),file=outfile,sep="\n")
   return(outfile)
 }
