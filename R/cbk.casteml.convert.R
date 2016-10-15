8ce1740d7ab7287d5a44cfe4de422b86bf2cf1cd
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2016-10-14 16:06:38 -0900

    Make filename simple, just with extension

--- R/cbk.casteml.convert.R
+++ R/cbk.casteml.convert.R
@@ -1,4 +1,9 @@
-#' Convert CASTEML files to temporary directory and return the file path
+#' @title Convert CASTEML file to CSV file
+#'
+#' @description Convert CASTEML file to CSV file.  The converted file
+#'   is stored in a temporary directory.  This function returns path
+#'   to the file.
+#' 
 #' @param pmlfile file path to CASTEML file
 #' @param category category to pass to `casteml convert'
 #' @return file path to converted file in temporary directory
@@ -8,7 +13,9 @@ cbk.casteml.convert <- function(pmlfile,category="trace") {
   ## EXAMPLES
   ## pmlfile <- cbk.casteml.download("20160627191317-464538")
   ## cbk.casteml.convert(pmlfile,category="trace")
-  outfile <- tempfile(pattern = sprintf("%s_%s@",tools::file_path_sans_ext(basename(pmlfile)),category), fileext=".dataframe")
+
+  ## outfile <- tempfile(pattern = sprintf("%s_%s@",tools::file_path_sans_ext(basename(pmlfile)),category), fileext=".dataframe")
+  outfile <- tempfile(fileext=".dataframe")
   ## system(paste("casteml convert -f dataframe -c ", category, pmlfile,">", outfile))
   cmd     <- paste("casteml convert -f dataframe -c ", category, pmlfile)
   cat(system(cmd, intern = TRUE),file=outfile,sep="\n")
--- R/cbk.casteml.download.R
+++ R/cbk.casteml.download.R
@@ -1,4 +1,8 @@
-#' Download analysis record in temporary directory and return the file path
+#' @title Download analysis record to CASTEML file
+#'
+#' @description Download analysis record to CASTEML file.  The
+#'   downloaded is stored in a temporary directory.  This function
+#'   returns path to the file.
 #' 
 #' @param arguments Arguments that will pass to `casteml download'.
 #'   Typically unique indentification number of stones in Medusa.
@@ -11,7 +15,9 @@ cbk.casteml.download <- function(arguments) {
   ## arguments <- c("20160627191317-464538","20160627191900-040404","20160627191919-895636")
   ## cbk.casteml.download(arguments)
   ## cbk.casteml.download("20160627191317-464538")
-  outfile <- tempfile(pattern = paste(arguments[1],"@",sep=""), fileext=".pml")
+
+  ## outfile <- tempfile(pattern = paste(arguments[1],"@",sep=""), fileext=".pml")
+  outfile <- tempfile(fileext=".pml")
   ## system(paste("casteml download",arguments[ii],">",outfile))
   cmd     <- paste("casteml download",arguments)
   cat(system(cmd, intern = TRUE),file=outfile,sep="\n")
