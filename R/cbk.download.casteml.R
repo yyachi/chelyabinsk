6fa03d82473dd40956bfbd46c796f5c87461ca70
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2016-10-19 09:25:00 +0900

    File name of CASTEML is made from command issued by md5

--- R/cbk.download.casteml.R
+++ R/cbk.download.casteml.R
@@ -2,11 +2,14 @@
 #'
 #' @description Download analysis records as a CASTEML file.  This
 #'   function returns path to the file.  The file is stored in a
-#'   temporary directory unless specified.
+#'   temporary directory unless specified.  Note with the same
+#'   arguments, this function downloads file only once per a R
+#'   session.
 #' 
 #' @param arguments Unique indentification number of stones in Medusa.
 #'   Really, those will pass to `casteml download'.
 #' @param file File path to save downloaded CASTEML file
+#' @param force Force download CASTEML file
 #' @return Path to CASTEML file that was downloaded in temporary
 #'   directory.
 #' @export
@@ -18,16 +21,20 @@
 #' pmlfile <- cbk.download.casteml(stone)
 #'
 #' pmlfile <- cbk.download.casteml("20081202172326.hkitagawa")
-cbk.download.casteml <- function(arguments,file=NULL) {
+cbk.download.casteml <- function(arguments,file=NULL,force=FALSE) {
+  cmd    <- paste(c("casteml download",arguments),collapse=" ")
 
   ## file <- tempfile(pattern = paste(arguments[1],"@",sep=""), fileext=".pml")
   ## system(paste("casteml download",arguments[ii],">",file))
   if(is.null(file)){
-    file <- tempfile(fileext=".pml")
-    ## file <- file.path(tempdir(),sprintf("%s%s",paste(arguments,collapse="&"),".pml")) # does not work on October 19, 2016
+    ## file <- tempfile(fileext=".pml")
+    file <- file.path(tempdir(),paste(digest::digest(cmd,algo='md5'),".pml",sep=""))
   }
 
-  cmd     <- paste(c("casteml download",arguments),collapse=" ")
-  cat(system(cmd, intern = TRUE),file=file,sep="\n")
+  ## Download file only when it does not exist
+  if (force || !file.exists(file)) {
+    cat(system(cmd, intern = TRUE),file=file,sep="\n")
+  }
+
   return(file)
 }
