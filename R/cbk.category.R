fb57464d2d65e24d03a2c0258fc14e38e31e7f45
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2016-11-27 19:07:49 +0900

    Change name of `cbk.return.category' to `cbk.category'

--- R/cbk.category.R
+++ R/cbk.category.R
@@ -2,8 +2,8 @@
 #' @return A list of categories that are acceptable by `cbk.plot'
 #' @export
 #' @examples
-#' categorylist <- cbk.return.category()
-cbk.return.category <- function(){
+#' categorylist <- cbk.category()
+cbk.category <- function(){
   categorylist = list(
     "trace",
     "lithium",
