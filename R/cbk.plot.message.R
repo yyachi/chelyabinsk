ffab59ff8e3d5600aa7b4b2a90549887f0fe6a40
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2016-12-01 21:50:50 +0900

    Accept long line

--- R/cbk.plot.message.R
+++ R/cbk.plot.message.R
@@ -1,28 +1,26 @@
 #' Display a diagram with message
+#' 
 #' @param pmlfile_or_stone A CASTEML file that exits locally or stone-ID (or pmlame)
 #' @param msg Message to be displayed on diagram
 #' @return Dataframe used to plot the diagram
 #' @export
 #' @examples
 #' pmlame <- cbk.read.casteml(cbk.path("20081202172326.hkitagawa.pml"),"ppm")
-#' tryCatch({plot(XX,YY)
-#' },error=function(msg){
-#' cbk.plot.message(pmlame,msg)})
+#' cbk.plot.message(pmlame,"Hello, world!")
 cbk.plot.message <- function(pmlfile_or_stone,msg) {
-  ## periodic <- cbk.periodic()
-  ## ref      <- cbk.ref("Wasson.1988","ug/g",cbk.periodic("atomicnumber"))
-  ## XX       <- 1:length(ref)
-  ## xlabel   <- names(ref)
-  ## YY       <- ref / ref
-  XX   <- 1:100
-  YY   <- rep(1,100)
-  ylim <- c(0,100)
-  plot(XX,YY,log="",type="o",lty=1,pch=4,col="red",ylim=ylim,
+  XX    <- 1:100
+  ## YY <- rep(1,100)
+  YY    <- 5*sin(XX/5) + 10
+  mylim <- c(0,100)
+  plot(XX,YY,type="l",lty=1,pch=4,col="red",xlim=mylim,ylim=mylim,
        xlab='',ylab='',xaxs='i',yaxs='i',axes=FALSE)
-  text(50,50,msg,cex=0.8)
-  ## axis(1,at=XX,labels=xlabel,cex.axis=0.9,las=2)
-  ## axis(2,axTicks(2),axTicks(2))
-  ## abline(h=1,lty=2)
+
+  ## text(50,50,msg,cex=0.8)
+  wrap_strings <- function(vector_of_strings,width){
+    as.character(sapply(vector_of_strings,FUN=function(x){paste(strwrap(x,width=width), collapse="\n")}))
+  }
+  text(3,97,wrap_strings(msg,40),adj=c(0,1))
+
   box(lwd=1)
   return(pmlfile_or_stone)
 }
