44679b8bdd461de0314fad48b4b9fd890e0d6f27
Masahiro Yamanaka <yamanaka@cc.okayama-u.ac.jp>
2016-12-07 18:59:08 +0900

    Add cbk.read.tblame

---
+++ R/cbk.read.tblame.R
@@ -0,0 +1,30 @@
+#' @title Read csvfile created by casteml convert with column `unit'
+#'
+#' @description Read csvfile with row `unit'.  
+#'
+#' @details This internally calls
+#'   \code{read.csv(tblame,row.names=1,header=T,stringsAsFactors=F)},
+#'   take out row of `unit' and normalized by the `unit' row.
+#' @param tblame A csvfile with columns of element abundances and
+#' rows of stones, with 2nd row `unit'
+#' @param tableunit Output unit that will be resolved by
+#'   cbk.convector() (default="none")
+#' @return A dataframe with unit organized
+#' @seealso \code{\link{cbk.read.dflame}},
+#'   \url{https://github.com/misasa/casteml},
+#'   \code{\link{cbk.convector}},
+#' @export
+cbk.read.tblame <- function(tblame,tableunit="none"){
+
+  ## cat(file=stderr(),"cbk.read.tblame: tblame is |",tblame,"|\n")
+  cat(file=stderr(),"cbk.read.tblame:32: tblame # =>",tblame,"\n")
+
+  pmlame <- read.csv(tblame,row.names=1,header=T,stringsAsFactors=F)
+  if ('unit' %in% rownames(pmlame)) {
+    factor                <- cbk.convector(as.matrix(pmlame['unit',]))
+    names(factor)         <- colnames(pmlame)
+    factor[is.na(factor)] <- 1
+    pmlame                <- as.data.frame(t(apply(pmlame[rownames(pmlame) != 'unit',],1,function(x) as.numeric(x) / factor ))* cbk.convector(tableunit))
+  }
+  return(pmlame)
+}
