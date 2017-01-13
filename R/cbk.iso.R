4c016a145588816a0d4facf7dacc4f2bb3694735
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2017-01-13 23:04:00 +0900

    Create interface for cbk.iso

--- R/cbk.iso.R
+++ R/cbk.iso.R
@@ -1,14 +1,23 @@
-#' @title Return isotope information
+#' @title Return properties of isotope
 #'
-#' @description Return isotope information.  It is in a form of pmlame.
+#' @description Return properties of isotope.  It is in a form of
+#'   pmlame.
 #'
-#' @return A pmlame of isotope information
-#' @seealso \code{\link{cbk.periodic}}
+#' @param isomeas A name of isotope such as `Li7' and `Rb87'.
+#' @param property A name of PROPERTY that is one of `proton',
+#'   `nucleon', `mass', or `ratio'
+#' @return A scalar of property or pmlame of isotope table
+#' @seealso \code{\link{cbk.periodic}}, isotope.m, isotope_constant.m
 #' @export
 #' @examples
-#' cbk.iso()['Li7','ratio']
-#' cbk.iso()['Li7','weight']
-cbk.iso <- function(){
-  isolame <- cbk.read.tblame(cbk.path("isotope-tblame0.csv"))
-  return(isolame)
+#' cbk.iso()
+#' cbk.iso('Li7')
+#' cbk.iso('Li7','ratio')
+cbk.iso <- function(isomeas=NULL,property='mass'){
+  isolame0 <- cbk.read.tblame(cbk.path("isotope-tblame0.csv"))
+  if (is.null(isomeas)) {
+    return(isolame0)
+  } else {
+    return(isolame0[isomeas,property])
+  }
 }
