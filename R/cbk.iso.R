c6eebeff2f1ec98f820d05a748218be1a80f4300
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2017-01-16 17:07:24 +0900

    Add pseudo atomic weight

--- R/cbk.iso.R
+++ R/cbk.iso.R
@@ -5,7 +5,7 @@
 #'
 #' @param isomeas A name of isotope such as `Li7' and `Rb87'.
 #' @param property A name of PROPERTY that is one of `proton',
-#'   `nucleon', `weight', `ratio', or `symbol'.
+#'   `nucleon', `weight', `ratio', `symbol', or `pseudo.atomic.weight'.
 #' @return A scalar of property or pmlame of isotope table
 #' @seealso \code{\link{cbk.periodic}}, isotope.m, isotope_constant.m
 #' @export
@@ -16,29 +16,43 @@
 cbk.iso <- function(isomeas=NULL,property='weight'){
   isolame0 <- cbk.read.tblame(cbk.path("isotope-tblame0.csv"),verbose=FALSE)
 
-  if (property %in% c('atomicnumber','Z','proton')) {
+  if (property %in% c('proton','atomicnumber','atomic.number',
+                      'atomic number','Z')) {
     property <- 'proton'
   }
 
-  if (property %in% c('iso','isonum','nucleon')) {
+  if (property %in% c('nucleon','iso','isonum')) {
     property <- 'nucleon'
   }
 
-  if (property %in% c('m','mass','weight')) {
+  if (property %in% c('weight','m','atomicweight','atomic.weight',
+                      'atomic weight','mass','atomicmass',
+                      'atomic.mass','atomic mass')) {
     property <- 'weight'
   }
 
-  if (property %in% c('ab','abundance','r','ratio')) {
+  if (property %in% c('ratio','ab','abundance','r')) {
     property <- 'ratio'
   }
 
-  if (property %in% c('element','sym','symbol')) {
+  if (property %in% c('symbol','element','sym')) {
     property <- 'symbol'
   }
-  
+
+  if (property %in% c('pseudo.atomic.weight','pseudo atomic weight',
+                      'pseudo atomicweight','pseudo.atomicweight')) {
+    property <- 'pseudo.atomic.weight'
+  }
+
   if (is.null(isomeas)) {
     return(isolame0)
+  } else if (property == 'pseudo.atomic.weight') {
+    myvect            <- isolame0[isolame0[isomeas,'symbol'],'weight']/isolame0[isomeas,'ratio']
+    names(myvect)     <- isomeas
+    return(myvect)
   } else {
-    return(isolame0[isomeas,property])
+    myvect            <- isolame0[isomeas,property]
+    names(myvect)     <- isomeas
+    return(myvect)
   }
 }
