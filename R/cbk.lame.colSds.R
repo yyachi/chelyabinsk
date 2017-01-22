66e352f7f885e6a68b941b3627d3ab82b0fe080f
Masahiro Yamanaka <yamanaka@cc.okayama-u.ac.jp>
2017-01-22 17:57:31 +0900

    Add new function `cbk.lame.merge.error'

--- R/cbk.lame.colSds.R
+++ R/cbk.lame.colSds.R
@@ -3,10 +3,25 @@
 #' @return A pmlame with standard deviation value of each column
 #' @export
 #' @examples
-#' pmlame  <- cbk.read.casteml(cbk.path("20081202172326.hkitagawa.pml"))
-#' pmlame0 <- pmlame[,colnames(pmlame) != "sample_id"]
+#' pmlame  <- cbk.read.casteml(cbk.path("20130528105235-594267.pml"))
+#' pmlame0 <- pmlame[,colnames(pmlame) != c("sample_id","image_id")]
 #' pmlame1 <- cbk.lame.colSds(pmlame0)
 cbk.lame.colSds <- function(pmlame) {
-  pmlame0 <- data.frame(t(apply(pmlame, 2, sd,na.rm=T)))
-  return(pmlame0)
+  delimiter <- "@|[.]|-[[:alnum:]]*$"
+  spotlist0 <- rownames(pmlame)
+  spotlist1 <- unlist(lapply(strsplit(spotlist0,delimiter),'[[',1))
+  stonelist <- unique(spotlist1)
+
+  pmlame2 <- NaN
+  for(ii in 1:length(stonelist)) {
+    matched <- grepl(stonelist[ii],spotlist0)
+    pmlame0 <- pmlame[matched,,drop=FALSE]
+    pmlame1 <- data.frame(t(apply(pmlame0, 2, sd,na.rm=T)))
+    pmlame2 <- rbind(pmlame2,pmlame1)
+  }
+  pmlame2           <- pmlame2[2:nrow(pmlame2),,drop=FALSE] # drop first row
+  rownames(pmlame2) <- stonelist
+
+  ## pmlame0 <- data.frame(t(apply(pmlame, 2, sd,na.rm=T)))
+  return(pmlame2)
 }
