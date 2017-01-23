621807e69cdab1dce593f6eb4659e6e627380194
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2017-01-23 10:03:19 +0900

    Revise to use cbk.lame.normalize

--- R/cbk.plot.ree.R
+++ R/cbk.plot.ree.R
@@ -27,8 +27,10 @@ cbk.plot.ree <- function(pmlfile_or_stone,opts=NULL,tableunit="none",property="a
   pmlame      <- cbk.read.casteml(pmlfile_or_stone,opts,tableunit)
   periodic    <- cbk.periodic()
   ref1        <- cbk.ref(reference,tableunit,cbk.periodic(property))
+
   stonelist   <- rownames(pmlame)
   stoneindex  <- 1:length(stonelist)
+
   chemlist    <- colnames(pmlame)
   REElist     <- c('La','Ce','Pr','Nd','Sm','Eu','Gd','Tb','Dy','Ho','Er','Tm','Yb','Lu');
   subchemlist <- intersect(REElist,chemlist)
@@ -38,27 +40,30 @@ cbk.plot.ree <- function(pmlfile_or_stone,opts=NULL,tableunit="none",property="a
   ## ----------------
   property0        <- periodic[subchemlist,property] # atomicnumber, volatility, compatibility
   names(property0) <- subchemlist
-  XX0              <- sort(property0)
-  ZZ               <- pmlame[,names(XX0),drop=FALSE]
-  CI               <- cbk.vector(ref1[names(XX0)])
-  YY               <- t(ZZ) / CI
+  
+  XX               <- sort(property0)
+  ZZ               <- pmlame[,names(XX),drop=FALSE]
+  CI               <- ref1[names(XX)]
+  ZZ1              <- cbk.lame.normalize(ZZ,CI)
+  YY               <- t(ZZ1)
 
   ## ----------------
   ##* PLOT
   ## ----------------
   ## par(mar=c(4.5,4.5,0.5,0.5),mfrow=c(2,1)) # c(bottom,left,top,right) c(5.1,4.1,4.1,2.1)
 
-  matplot(XX0,YY,log="y",type="o",lty=1,pch=stoneindex,col=stoneindex,
+  matplot(XX,YY,log="y",type="o",lty=1,pch=stoneindex,col=stoneindex,
           xlab='',ylab='ZZ/CI',axes=FALSE)
-  axis(1,at=XX0,labels=names(XX0),cex.axis=0.9,las=2)
+  axis(1,at=XX,labels=names(XX),cex.axis=0.9,las=2)
   axis(2,axTicks(2),axTicks(2))
   abline(h=1,lty=2)
   box(lwd=1)
   if (opts$legendp) {
     legend('bottomright',stonelist,lty=1,pch=stoneindex,col=stoneindex,ncol=4,cex=0.5)
   }
+
   ## ----------------
   ##* CLOSING REMARK
   ## ----------------
-  return(ZZ)
+  return(ZZ1)
 }
