0a70648684a540668c4f0180e851a88c9ed54202
Masahiro Yamanaka <yamanaka@cc.okayama-u.ac.jp>
2017-01-25 20:40:06 +0900

    Add error bar

--- R/cbk.plot.ree.R
+++ R/cbk.plot.ree.R
@@ -27,19 +27,29 @@ cbk.plot.ree <- function(pmlfile_or_stone,opts=NULL,tableunit="none",reference="
   ##* OPENING REMARK
   ## ----------------
   ## pmlame <- cbk.read.casteml(pmlfile,tableunit,category="trace")
-  pmlame0   <- cbk.read.casteml(pmlfile_or_stone,opts,tableunit)
-  stonelist <- rownames(pmlame0)
+  pmlame0              <- cbk.read.casteml(pmlfile_or_stone,opts,tableunit)
+  stonelist            <- rownames(pmlame0)
 
-  reflame   <- cbk.ref(reference,tableunit)
-  pmlame1   <- cbk.lame.normalize(pmlame0,reflame)
+  reflame              <- cbk.ref(reference,tableunit)
+  pmlame1              <- cbk.lame.normalize(pmlame0,reflame)
+
+  errorlist            <- grep("_error",colnames(pmlame0),value=T)
+  errorlame0           <- pmlame0[errorlist]
+  colnames(errorlame0) <- unlist(lapply(strsplit(errorlist,"_error"),'[[',1))
+  errorlame1           <- cbk.lame.normalize(errorlame0,reflame)
 
   ## ----------------
   ##* PARSE
   ## ----------------
-  chemlist  <- intersect(c('La','Ce','Pr','Nd','Sm','Eu','Gd','Tb','Dy','Ho','Er','Tm','Yb','Lu'), colnames(pmlame1));
-  XX        <- cbk.periodic("atomicnumber")[chemlist]
-  pmlame9   <- pmlame1[,names(XX),drop=FALSE]
-  YY        <- t(pmlame9)
+  chemlist   <- intersect(c('La','Ce','Pr','Nd','Sm','Eu','Gd','Tb','Dy','Ho','Er','Tm','Yb','Lu'), colnames(pmlame1));
+  XX         <- cbk.periodic("atomicnumber")[chemlist]
+  pmlame9    <- pmlame1[,names(XX),drop=FALSE]
+  YY         <- t(pmlame9)
+
+  if (length(errorlist) != 0) {
+    errorlame9 <- errorlame1[,names(XX),drop=FALSE]
+    YY_sd      <- t(errorlame9)
+  }
 
   ## ----------------
   ##* PLOT
@@ -57,6 +67,13 @@ cbk.plot.ree <- function(pmlfile_or_stone,opts=NULL,tableunit="none",reference="
 
   matplot(XX,YY,log="y",type="o",lty=1,pch=pch,col=col,
           xlab='',ylab='ZZ/CI',axes=FALSE)
+  if (length(errorlist) != 0) {
+    errorbar.y <- function(x,y,err,w,col=1){x0=x;y0=y-err;x1=x;y1=y+err;arrows(x0,y0,x1,y1,code=3,angle=90,length=w,col=col);}
+    for(ii in 1:length(stonelist)) {
+      errorbar.y(XX,YY[,ii],YY_sd[,ii],
+                 0.05,col=col[ii])
+    }
+  }
   axis(1,at=XX,labels=names(XX),cex.axis=0.9,las=2)
   axis(2,axTicks(2),axTicks(2))
   abline(h=1,lty=2)
