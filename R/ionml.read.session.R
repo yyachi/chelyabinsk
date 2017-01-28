b0360690126fea68106be6e9e167e9058765daa1
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2017-01-28 19:26:21 +0900

    Add option to read mean of ion intensity instead of normalized one

--- R/ionml.read.session.R
+++ R/ionml.read.session.R
@@ -9,8 +9,10 @@
 #' @param t2 Time when ion starts (default: 25 s).
 #' @param t3 Time when ion ends (default: 60 s).
 #' @param ref Reference ion such as `Si29'.
-#' @param DL Overwrite `stderr' by delection limit (DL) when DL is
-#'   larger than `stderr'.
+#' @param DL Use delection limit (DL) instead of `sderr' when DL is
+#'   larger than `sderr'.
+#' @param intensityp Have mean and sd of intensity instead of mean and
+#'   sderr of ionic ratio
 #' @return Pseudo-pmlame with average and standard error of ionic
 #'   ratio
 #' @seealso \code{\link{ionml.read.laicpqms}}
@@ -18,45 +20,51 @@
 #' @examples
 #' files <- c(cbk.path("ref_cpx_klb1@1.ion"),cbk.path("ref_cpx_klb1@2.ion"),cbk.path("ref_cpx_klb1@3.ion"))
 #' pmlame0 <- ionml.read.session(files)
-ionml.read.session <- function(tblame.csv,t0=5,t1=20,t2=25,t3=60,ref="Si29",DL=FALSE) {
+ionml.read.session <- function(tblame.csv,t0=5,t1=20,t2=25,t3=60,ref="Si29",DL=FALSE,intensityp=FALSE) {
   ## cat(file=stderr(),"ionml.read.session:22: t0 # =>",t0,"\n")
   ## cat(file=stderr(),"ionml.read.session:23: t1 # =>",t1,"\n")
   ## cat(file=stderr(),"ionml.read.session:24: t2 # =>",t2,"\n")
   ## cat(file=stderr(),"ionml.read.session:25: t3 # =>",t3,"\n")
 
-  pmMean0               <- data.frame()
-  pmSderr0              <- data.frame()
+  meanlame                     <- data.frame()
+  errorlame                    <- data.frame()
 
   for(acq_ii in tblame.csv){
-    acqname             <- tools::file_path_sans_ext(basename(acq_ii))
-    ## pmStat1          <- ionml.read.laicpqms(acq_ii,ref=ref)
-    pmStat1             <- ionml.read.laicpqms(acq_ii,t0=t0,t1=t1,t2=t2,t3=t3,ref=ref)
-    pmStat2             <- pmStat1[,colnames(pmStat1)!="time"] # drop column of `time'
+    ##* Load data
+    acqName                    <- tools::file_path_sans_ext(basename(acq_ii))
+    ## ionlame0                <- ionml.read.laicpqms(acq_ii,ref=ref)
+    ionlame0                   <- ionml.read.laicpqms(acq_ii,t0=t0,t1=t1,t2=t2,t3=t3,ref=ref)
+    ionlame                   <- ionlame0[,colnames(ionlame0)!="time"] # drop column of `time'
 
-    pmMean2             <- pmStat2[paste0("mean/",ref),]
-    row.names(pmMean2)  <- acqname
-    pmMean0             <- rbind(pmMean0, pmMean2)
-
-    pmSderr2            <- pmStat2[paste0("sderr/",ref),] # sderr
-
-    if (DL) { # Replace stderr by detection limit, if larger
-      pmDL2                    <- pmStat2[paste0("DL/",ref),]
-      idx_overwrite            <- pmDL2 > pmSderr2
-      pmSderr2[,idx_overwrite] <- pmDL2[,idx_overwrite] # DL instead of sderr
+    ##* Deal mean
+    acqMean                    <- ionlame[paste0("mean/",ref),] # normalized intensity
+    if (intensityp) {
+      acqMean                  <- ionlame["mean/cps",] # raw intensity
     }
+    row.names(acqMean)         <- acqName
+    meanlame                   <- rbind(meanlame, acqMean)
 
-    row.names(pmSderr2) <- acqname
-    pmSderr0            <- rbind(pmSderr0, pmSderr2)
+    ##* Deal error
+    acqError                   <- ionlame[paste0("sderr/",ref),] # sderr
+    if (intensityp) {
+      acqError                 <- ionlame["sd/cps",]
+    } else if (DL) { # Replace sderr by detection limit, if larger
+      acqDL                    <- ionlame[paste0("DL/",ref),]
+      idx_overwrite            <- acqDL > acqError
+      acqError[,idx_overwrite] <- acqDL[,idx_overwrite] # DL instead of sderr
+    }
+    row.names(acqError)        <- acqName
+    errorlame                  <- rbind(errorlame, acqError)
   }
 
-  # Rename column of standard-error to be similar to CASTEML
-  colnames(pmSderr0)    <-paste0(colnames(pmSderr0),"_error")
+  ##* Rename column of error to be similar to CASTEML
+  colnames(errorlame) <-paste0(colnames(errorlame),"_error")
 
-  # Merge columns of mean and standard-error interleavely
-  pmStat1               <- cbind(pmMean0,pmSderr0)
-  n                     <- ncol(pmMean0)
-  index                 <- rep(1:n, each = 2) + (0:1) * n
-  pmStat2               <- pmStat1[index]
+  ##* Merge columns of mean and standard-error interleavely
+  fullame             <- cbind(meanlame,errorlame)
+  n                   <- ncol(meanlame)
+  index               <- rep(1:n, each = 2) + (0:1) * n
+  fullame1            <- fullame[index]
 
-  return(pmStat2)
+  return(fullame1)
 }
