382e7c6b2363ea0f28732f82cc354b7bf537c823
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2017-01-28 18:09:17 +0900

    Re-organize arguments and accept sputter.rate instead of dimensions of pit

--- R/cbk.lame.delivery.R
+++ R/cbk.lame.delivery.R
@@ -1,29 +1,25 @@
 #' @title Estimate delivery rate of ion from a pit by laser abbration
 #' @description Estimate delivery rate of ion from a pit by laser
-#'   abbration.  Although dimension of a pit is on meter [m],
-#'   internaly, they are are converted into centi-meter [cm].
+#'   abbration.  Calculation is made in dimension of CGS.
 #' @param pmlame A pmlame with row of stone and column of chem [g/g].
 #' @param intlame A pseudo-pmlame of ion intensity with row of stone
 #'   and column of isomeas [cps].
-#' @param radius Radius of a pit [m] (default: 12.5e-6).
-#' @param density Density of solid target [g/cc] (default: 3).
-#' @param depth Depth of a pit [m] (default: 30e-6).
-#' @param t Period of irradiation [s] (default: 40).
+#' @param sputter.rate Sputter rate [g/s] (default: 1.1e-9 that
+#'   corresponds to abbration of 40 s to produce a pit with radius of
+#'   12.5 micron, depth of 30 micron, and density of 3 g/cc).
 #' @return Delivery rate from solid target to detector.
 #' @export
 #' @examples
-#' intlame0 <- data.frame(row.names=c("ref_gl_tahiti@1","ref_cpx_klb1@1"),Si29=c(3.85e+05,3.95e+05),Li7=c(1.87e+03,1.73e+02),Sr88=c(3.24e+04,1.50e+04))
-#' pmlame0 <- data.frame(row.names=c("ref-gl-tahiti","ref-cpx-klb1"),SiO2=c(0.590,0.520),Li=c(20.8e-06,1.37e-06),Sr=c(107e-06,36.1e-06))
-#' pmlame1 <- cbk.lame.reduce(pmlame0)
-#' cbk.lame.delivery(pmlame1,intlame0)
-cbk.lame.delivery <- function(pmlame,intlame,radius=12.5e-6,density=3,depth=30e-6,t=40){
-
-  ##* Estimate sputter rate
-  radius1      <- radius * 100                  # radius of a pit [cm]
-  depth1       <- depth * 100                   # depth of a pit [cm]
-  vol.pit      <- 3.141592 * radius1^2 * depth1 # volume sputtered [cc]
-  num_a        <- 6.022e23                      # Avogadro constant [/mol]
-  mass.pit     <- density * vol.pit
+#' intlame0     <- data.frame(row.names=c("ref_gl_tahiti@1","ref_cpx_klb1@1"),Si29=c(3.85e+05,3.95e+05),Li7=c(1.87e+03,1.73e+02),Sr88=c(3.24e+04,1.50e+04))
+#' pmlame0      <- data.frame(row.names=c("ref-gl-tahiti","ref-cpx-klb1"),SiO2=c(0.590,0.520),Li=c(20.8e-06,1.37e-06),Sr=c(107e-06,36.1e-06))
+#' pmlame1      <- cbk.lame.reduce(pmlame0)
+#' radius       <- 12.5e-6 # Radius of a pit [m]
+#' depth        <- 30e-6   # Depth of a pit [m]
+#' t            <- 40      # Period of irradiation [s]
+#' density      <- 3       # Density of solid target [g/cc]
+#' sputter.rate <- 3.14 * (radius * 100)^2 * (depth * 100) * density / t # [g/s]
+#' cbk.lame.delivery(pmlame1,intlame0,sputter.rate)
+cbk.lame.delivery <- function(pmlame,intlame,sputter.rate=1.1e-09){
 
   ##* Rename rownames to be with "_" but "-"
   acqlist0          <- rownames(intlame)
@@ -31,6 +27,7 @@ cbk.lame.delivery <- function(pmlame,intlame,radius=12.5e-6,density=3,depth=30e-
   rownames(intlame) <- gsub("-","_",rownames(intlame))
 
   ##* Setup
+  num_a        <- 6.022e23 # Avogadro constant [/mol]
   isomeas      <- colnames(intlame)
   chemlist     <- cbk.iso(isomeas,'symbol')
   acqlist      <- rownames(intlame)
@@ -44,7 +41,7 @@ cbk.lame.delivery <- function(pmlame,intlame,radius=12.5e-6,density=3,depth=30e-
                                length(stonelist),'v')
 
   ##* Real work
-  yield <- intlame / (mass.pit / t * pmlame1 / pseudo.wt * num_a)
+  yield <- intlame / (sputter.rate * pmlame1 / pseudo.wt * num_a)
 
   ##* Restore rownames
   rownames(yield) <- acqlist0
