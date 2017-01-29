4032df4067d7285016f68bd01a4eb6b8010a7dc9
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2017-01-29 10:46:08 +0900

    Support different format such for tex

--- R/cbk.convert.casteml.R
+++ R/cbk.convert.casteml.R
@@ -1,17 +1,18 @@
-#' @title Convert CASTEML file to CSV file
+#' @title Convert CASTEML file to certain file
 #'
-#' @description Convert CASTEML file to CSV file.  This function
+#' @description Convert CASTEML file to certain file.  This function
 #'   returns path to the converted file.  The converted file is stored
 #'   in a temporary directory.  Note with the same arguments, this
 #'   function convert file only once per a R session.  This is
 #'   low-level function and users are not encourage to call this but
 #'   cbk.read.casteml().
 #'
-#' @param pmlfile File path to CASTEML file
-#' @param category Category filter that is passed to `casteml convert'
-#' @param force Flag to force convert again
+#' @param pmlfile File path to CASTEML file.
+#' @param category Category filter that is passed to `casteml convert'.
+#' @param force Flag to force convert again.
 #' @param verbose Output debug info (default: TRUE).
-#' @return File path to the converted file
+#' @param format Output format (default: dataframe).
+#' @return Path of the converted file.
 #' @export
 #' @seealso \code{casteml convert} and
 #'   \url{https://github.com/misasa/casteml}
@@ -20,22 +21,28 @@
 #' dflame.csv <- cbk.convert.casteml(pmlfile)
 #'
 #' pmlfile    <- cbk.download.casteml("20081202172326.hkitagawa")
-#' dflame.csv <- cbk.convert.casteml(pmlfile,category="trace")
-cbk.convert.casteml <- function(pmlfile,category=NULL,force=FALSE,verbose=TRUE) {
-  ## cat(file=stderr(),"cbk.convert.casteml: pmlfile is |",pmlfile,"|\n")
-  if (verbose) {
-    cat(file=stderr(),"cbk.convert.casteml:25: pmlfile # =>",pmlfile,"\n")
-  }
-  ## outfile <- tempfile(pattern = sprintf("%s_%s@",tools::file_path_sans_ext(basename(pmlfile)),category), fileext=".dflame")
-  ## outfile <- tempfile(fileext=".dflame")
-  if(is.null(category)){
-    cmd     <- paste("convert -f dataframe",pmlfile)
-  } else {
-    cmd     <- paste("convert -f dataframe -c",category,pmlfile)
-  }
-  outfile <- file.path(tempdir(),paste0(digest::digest(cmd,algo='md5'),".dflame"))
+#' texfile    <- cbk.convert.casteml(pmlfile,format="tex")
+cbk.convert.casteml <- function(pmlfile,category=NULL,force=FALSE,verbose=TRUE,format="dataframe") {
 
-  ## Convert file only when it does not exist
+  ##* Console
+  if (verbose) {
+    cat(file=stderr(),"cbk.convert.casteml:29: pmlfile # =>",pmlfile,"\n")
+  }
+
+  ##* Set extension list
+  extlist        <- c(".dflame",".tex")
+  names(extlist) <- c("dataframe","tex")
+  ext            <- extlist[format]
+
+  ##* Prepare output file
+  if(is.null(category)){
+    cmd     <- paste("convert -f",format,pmlfile)
+  } else {
+    cmd     <- paste("convert -f",format,"-c",category,pmlfile)
+  }
+  outfile <- file.path(tempdir(),paste0(digest::digest(cmd,algo='md5'),ext))
+
+  ##* Convert file (only when it does not exist)
   if (force || !file.exists(outfile)) {
     ## system(cmd,">",outfile))
     ## cat(system(cmd,intern=TRUE),file=outfile,sep="\n") # intern=T accepts 8095 char max per a line
@@ -44,5 +51,6 @@ cbk.convert.casteml <- function(pmlfile,category=NULL,force=FALSE,verbose=TRUE) 
     }
     system2("casteml",cmd,stdout=outfile)
   }
+
   return(outfile)
 }
