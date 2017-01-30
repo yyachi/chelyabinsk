7a540f3cab4afff1d8f1c914c5001d3f7b5c10d5
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2017-01-30 11:42:40 +0900

    Rename cbk.lame.texfy to cbk.texfy to accpet pmlfile_or_stone

--- R/cbk.texfy.R
+++ R/cbk.texfy.R
@@ -1,8 +1,9 @@
-#' @title Convert pmlame to multple texfiles
+#' @title Convert CASTEML file to multple texfiles
 #'
-#' @description Convert pmlame to multple texfiles.  This function
-#'   split chem and parse the datasets separatedly.
-#' @param pmlame A pmlame with row of stone and column of chem [g/g].
+#' @description Convert CASTEML file to multple texfiles.  This
+#'   function splits chem and parse the datasets separatedly.
+#' @param pmlfile_or_stone A CASTEML file that exits locally or
+#'   stone-ID (or pmlame).
 #' @param outfile File path to texfile.
 #' @param chem List of chem such as c("Li","Si","Ca","Ca.1","Rb").
 #' @param ncol Number of columns per a table.
@@ -10,14 +11,14 @@
 #' @return Vector of file path to texfiles.
 #' @seealso \code{\link{cbk.lame.texfy1}}
 #' @export
-cbk.lame.texfy <- function(pmlame,outfile="table-auto.tex",chem=NULL,ncol=11,verbose=FALSE) {
+cbk.texfy <- function(pmlfile_or_stone,outfile="table-auto.tex",chem=NULL,ncol=11,verbose=FALSE) {
 
   if (verbose) {
     cat(file=stderr(),"cbk.lame.texfy:16: outfile # =>",outfile,"\n")
   }
 
   ## pmlame <- cbk.lame.regulate(pmlame)
-  pmlame <- cbk.lame.regulate(cbk.read.casteml(pmlame))
+  pmlame <- cbk.lame.regulate(cbk.read.casteml(pmlfile_or_stone))
 
   if (is.null(chem)) {
     chem <- grep("_error$",colnames(pmlame),value=T,invert=T)
