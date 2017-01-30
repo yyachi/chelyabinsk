20e0ed93e3b417de28d16c74f39a47a792117a0a
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2017-01-30 11:35:42 +0900

    Accept pmlfile

--- R/cbk.lame.texfy.R
+++ R/cbk.lame.texfy.R
@@ -7,7 +7,7 @@
 #' @param chem List of chem such as c("Li","Si","Ca","Ca.1","Rb").
 #' @param ncol Number of columns per a table.
 #' @param verbose Output debug info.
-#' #' @return Vector of file path to texfiles.
+#' @return Vector of file path to texfiles.
 #' @seealso \code{\link{cbk.lame.texfy1}}
 #' @export
 cbk.lame.texfy <- function(pmlame,outfile="table-auto.tex",chem=NULL,ncol=11,verbose=FALSE) {
@@ -16,7 +16,8 @@ cbk.lame.texfy <- function(pmlame,outfile="table-auto.tex",chem=NULL,ncol=11,ver
     cat(file=stderr(),"cbk.lame.texfy:16: outfile # =>",outfile,"\n")
   }
 
-  pmlame <- cbk.lame.regulate(pmlame)
+  ## pmlame <- cbk.lame.regulate(pmlame)
+  pmlame <- cbk.lame.regulate(cbk.read.casteml(pmlame))
 
   if (is.null(chem)) {
     chem <- grep("_error$",colnames(pmlame),value=T,invert=T)
