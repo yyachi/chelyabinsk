4d41fc0a17b6f7366d6e9f81e0d0f794d8610ba0
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2017-02-03 14:28:04 +0900

    Accept more than two pmlames

--- R/cbk.lame.merge.R
+++ R/cbk.lame.merge.R
@@ -1,37 +1,44 @@
 #' Merge to two pmlames
-#' @param pmlame1 A pmlame with row of stone and column of chem [g/g].
-#' @param pmlame2 Another pmlame.
+#' @param ... Multiple pmlames with row of stone and column of chem [g/g].
 #' @param verbose Output debug info (default: FALSE).
-#' @return A combined pmlame with row of stone and column of chem
+#' @return A merged pmlame with row of stone and column of chem.
 #' @seealso \code{cbk.lame.merge.error} and \code{merge}
 #' @export
 #' @examples
 #' pmlame1 <- data.frame(row.names=c("stone@1","stone@2"), SiO2=c(0.525,0.420), fo=c(0.353,3.400), d7Li=c(1.0,3.3))
 #' pmlame2 <- structure(list(fo = c(0.98,0.74), d18O = c(4.8,2.5)), row.names = c("ol_chd1@5","cpx_chd1@6"), .Names = c("fo","d18O"), class = "data.frame")
 #' cbk.lame.merge(pmlame1,pmlame2)
-cbk.lame.merge <- function(pmlame1,pmlame2,verbose=FALSE) {
+cbk.lame.merge <- function(...,verbose=FALSE) {
+  pmlames <- list(...)
 
   if(verbose){
-    cat(file=stderr(),"cbk.lame.merge:11: pmlame1 <- ")
-    cbk.lame.stringfy(pmlame1)
-    cat(file=stderr(),"cbk.lame.merge:13: pmlame2 <- ")
-    cbk.lame.stringfy(pmlame2)
+    for(ii in 1:length(pmlames)) {
+      cat(file=stderr(),sprintf("cbk.lame.merge:16: pmlames[[%d]] <- ",ii))
+      cbk.lame.stringfy(pmlames[[ii]])
+    }
   }
 
-  stone1 <- rownames(pmlame1)
-  chem1  <- colnames(pmlame1)
-  stone2 <- rownames(pmlame2)
-  chem2  <- colnames(pmlame2)
+  stone  <- c();
+  chem   <- c();
 
-  stone  <- union(rownames(pmlame1),rownames(pmlame2))
-  chem   <- union(colnames(pmlame1),colnames(pmlame2))
+  for(lame in pmlames) {
+    stone_i <- rownames(lame)
+    chem_i  <- colnames(lame)
+
+    stone <- union(stone,stone_i)
+    chem  <- union(chem,chem_i)
+  }
 
   pmlame           <- as.data.frame(matrix(data=NA,nrow=length(stone),ncol=length(chem)))
   rownames(pmlame) <- stone
   colnames(pmlame) <- chem
 
-  pmlame[stone1,chem1] <- pmlame1[stone1,chem1]
-  pmlame[stone2,chem2] <- pmlame2[stone2,chem2]
+  for(lame in pmlames) {
+    stone_i <- rownames(lame)
+    chem_i  <- colnames(lame)
+
+    pmlame[stone_i,chem_i] <- lame[stone_i,chem_i]
+  }
 
   return(pmlame)
 }
