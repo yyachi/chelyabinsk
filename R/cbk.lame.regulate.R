af5360e76bae8d322f4cb329d7dbddcf32ffccf0
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2017-02-15 16:06:36 +0900

    Change location of column filter.

--- R/cbk.lame.regulate.R
+++ R/cbk.lame.regulate.R
@@ -6,7 +6,9 @@
 #'   errorlame.  To get errorlame, use \code{cbk.lame.fetch.error}
 #'   instead.
 #' @param extra Flag to extract extra columns (default: FALSE).
-#' @param chem Target chem you want to extract
+#' @param chem Select only certain chem for mean and error.  With chem
+#'   c('Li','Sr88'), columns of 'Li','Li_error','Sr88','Sr88_error'
+#'   are selected.
 #' @return A pmlame with columns of concern
 #' @seealso \code{\link{cbk.lame.fetch.error}}
 #' @export
@@ -14,6 +16,10 @@
 #' pmlame  <- cbk.read.casteml(cbk.path("20130528105235-594267.pml"))
 #' pmlame1 <- cbk.lame.regulate(pmlame)
 cbk.lame.regulate <- function(pmlame,mean=TRUE,error=TRUE,extra=FALSE,chem=NULL) {
+
+  if (!is.null(chem)) {
+    pmlame <- pmlame[,c(chem,paste0(chem,"_error")),drop=FALSE]
+  }
 
   col0     <- colnames(pmlame)
   colError <- grep("_error$",col0,value=T)
@@ -34,10 +40,5 @@ cbk.lame.regulate <- function(pmlame,mean=TRUE,error=TRUE,extra=FALSE,chem=NULL)
   col1 <- setdiff(col0,colDrop)
   pmlame1 <- pmlame[,col1,drop=FALSE]
 
-  if (!is.null(chem)) {
-    col2    <- c(chem,paste0(chem,"_error"))
-    colidx  <- match(col2,colnames(pmlame1))
-    pmlame1 <- pmlame1[,sort(colidx),drop=FALSE]
-  }
   return(pmlame1)
 }
