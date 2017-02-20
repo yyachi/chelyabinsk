902e9ef403aeb3a4a66994e83374e4a027b1a0b9
Masahiro Yamanaka <yamanaka@cc.okayama-u.ac.jp>
2017-02-20 12:14:22 +0900

    Add a new function `cbk.read.ionml'

---
+++ R/cbk.read.ionml.R
@@ -0,0 +1,39 @@
+#' @title Read IONML file and return an ionlame
+#'
+#' @param ionml.xml An IONML file created in advance using appropriate
+#'   script dedicated for device.
+#' @param verbose Output debug info (default: TRUE).
+#' @return A dataframe with mean and time columns.
+#' @seealso \code{\link{ionml.convert.laicpqms}},
+#'   \code{ionml.convert.iontblame}, and
+#'   \url{https://github.com/misasa/casteml}
+#' @export
+#' @examples
+#' ionlame    <- cbk.read.ionml(cbk.path("ref_cpx_klb1@1.xml"))
+cbk.read.ionml <- function(ionml.xml,verbose=TRUE){
+  library(XML)
+
+  if (verbose) {
+    cat(file=stderr(),"cbk.read.ionml:17: ionml.xml # =>",ionml.xml,"\n")
+  }
+
+  doc   <- xmlParse(ionml.xml)
+  nodes <- getNodeSet(doc, "//target")
+
+  meanlame0 <- as.data.frame(sapply(nodes,xpathSApply,"./data",xmlValue),row.names=TRUE)
+  timelame0 <- as.data.frame(sapply(nodes,xpathSApply,"./data",xmlAttrs),row.names=TRUE)
+  names     <- sapply(nodes,xpathSApply,"./name",xmlValue)
+  ## isomeas   <- unlist(lapply(strsplit(names,"^int_"),'[[',2))
+  isomeas   <- gsub("^int_","",names)
+
+  colnames(meanlame0) <- isomeas
+  colnames(timelame0) <- paste0(isomeas,"_time")
+
+  ionlame1 <- cbind(timelame0,meanlame0)
+  n        <- ncol(meanlame0)
+  index    <- rep(1:n, each = 2) + (0:1) * n
+  ionlame2 <- ionlame1[index]
+
+  ionlame3 <- as.data.frame(apply(ionlame2,c(1,2),as.numeric))
+  return(ionlame3)
+}
