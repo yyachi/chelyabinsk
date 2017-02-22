837ee97ee3a21ccd53da418fdc3279ce06589fa7
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2017-02-22 14:16:25 +0900

    Default round to 6 digit unless specified.

--- R/cbk.write.tblame.R
+++ R/cbk.write.tblame.R
@@ -1,13 +1,22 @@
 #' Write pmlame as TBLAME.csv
 #'
 #' @param pmlame A pmlame with row of stone and column of chem [g/g].
-#' @param outfile Path of TBLAME.csv, which is a csvfile with rows of stone
-#'   and columns of chem.
+#' @param outfile Path of TBLAME.csv, which is a csvfile with rows of
+#'   stone and columns of chem.
+#' @param digit Integer indicating the number of significant digits to
+#'   be used by \code{signif}.
+#' @param verbose Output debug info (default: FALSE).
 #' @return Path of TBLAME.csv
 #' @seealso \code{\link{write.csv}}, \code{\link{cbk.read.tblame}},
 #'   and \url{https://github.com/misasa/casteml}
 #' @export
-cbk.write.tblame <- function(pmlame,outfile){
+cbk.write.tblame <- function(pmlame,outfile,digit=6,verbose=TRUE){
+
+  # Round number
+  if(!is.null(digit)){
+    pmlame <- signif(pmlame,digit)
+  }
+
   write.csv(pmlame,outfile)
   return(outfile)
 }
