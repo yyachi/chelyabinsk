c707401cb9a9c943a6e3b6ef70157411a72423e7
Tak Kunihiro <tkk@misasa.okayama-u.ac.jp>
2018-05-29 14:33:10 +0900

    Revise documentation to describe related functions

--- R/cbk.read.pmlame.R
+++ R/cbk.read.pmlame.R
@@ -1,15 +1,24 @@
-#' @title Get and return a pmlame
+#' @title Return a pmlame that is stored in Medusa
 #'
-#' @description Get and return a pmlame.  This function can use two
-#'   pathways to get a pmlame.  The one downloads a json data from
-#'   medusa, while the other downloads a CASTEML file using
-#'   rubygem. Finally, they are converted to pmlames with same
-#'   structure.
+#' @description Return a pmlame that is stored in Medusa.  There can
+#'   be two approach to get the pmlame.  (1) The first approach is to
+#'   downloads a CASTEML file using external program `gem package --
+#'   casteml'.  (2) The other approach is to download a json data
+#'   directoly from Medusa.
+#'
+#'   The essential functions involved in this function as of May 29
+#'   (2018) are shown below.
+#' 
+#'   cbk.read.pmlame <returns pmlame>
+#'   --> cbk.read.casteml <returns pmlame>
+#'       --> cbk.download.casteml <returns pmlfile>
+#'   --> cbk.download.pmlame <returns pmlame>
+#'       --> medusaRClient.read.pmlame <returns pmlame>
 #' 
 #' @param pmlfile_or_stone A CASTEML file that exists locally or
 #'   stone-ID.
-#' @param use_gem Download a CASTEML file into a temporary directory
-#'   (default: FALSE).
+#' @param casteml Download a CASTEML file using external program `gem package --
+#'   casteml' (default: FALSE).
 #' @param opts List of further options for plot.
 #' @param verbose Output debug info (default: TRUE).
 #' @return A dataframe with unit organized.
@@ -22,7 +31,7 @@
 #' pmlame  <- cbk.read.pmlame(pmlfile,tableunit="ppm",category="trace")
 #' stone   <- "20081202172326.hkitagawa"
 #' pmlame  <- cbk.read.pmlame(stone,tableunit="ppm")
-cbk.read.pmlame <- function(pmlfile_or_stone,opts=NULL,use_gem=FALSE,verbose=TRUE,...){
+cbk.read.pmlame <- function(pmlfile_or_stone,opts=NULL,casteml=FALSE,verbose=TRUE,...){
   opts_default <- list(Recursivep=FALSE)
   opts_default[intersect(names(opts_default),names(opts))] <- NULL  ## Reset shared options
   opts <- c(opts,opts_default)
@@ -33,7 +42,7 @@ cbk.read.pmlame <- function(pmlfile_or_stone,opts=NULL,use_gem=FALSE,verbose=TRU
         ifelse(is.data.frame(pmlfile_or_stone),"#<pmlame>",pmlfile_or_stone),"\n")
   }
 
-  if (use_gem || is.data.frame(pmlfile_or_stone) || file.exists(pmlfile_or_stone)) {
+  if (casteml || is.data.frame(pmlfile_or_stone) || file.exists(pmlfile_or_stone)) {
     pmlame <- cbk.read.casteml(pmlfile_or_stone,opts,verbose=verbose,...)
   } else {
     pmlame <- cbk.download.pmlame(pmlfile_or_stone,opts,verbose=verbose,...)
